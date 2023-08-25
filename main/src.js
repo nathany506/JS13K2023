ad919dcb65e80c06a48ff9b21f61bdc311761ef7
Craiky <31390334+Craiky@users.noreply.github.com>
2023-08-25 13:43:49 -0700

    Setup
    Updated my setup from last year and set everything up.

---
+++ main/src.js
@@ -0,0 +1,10 @@
+//Where to store spritesheet
+const spriteSheet = new Image
+
+//Game Canvas
+let canvas = document.getElementById("c")
+let ctx = canvas.getContext("2d")
+
+//Buffer Canvas
+let canv = document.getElementById("g")
+let gfx = canv.getContext("2d")
